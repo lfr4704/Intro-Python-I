af240e2b8f72698c7c38717a9f08261b001c4f38
Luis Robles <lfr4704@gmail.com>
2020-09-06 12:23:29 -0700

    completed 10

--- src/10_functions.py
+++ src/10_functions.py
@@ -3,10 +3,24 @@
 # YOUR CODE HERE
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+num = None
+def take_input():
+    num = input("Enter a number: ")
+    num = int(num)
+    if not isinstance(num, int):
+        print("This entry is invalid")
+    return num
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+def check_if_even(num):
+    if num % 2 == 0:
+        print(f'{num} is a even number!')
+    else:
+        print(f'{num} is not an even number :(!')
+    return num
+
+take_input()
+check_if_even(num)
